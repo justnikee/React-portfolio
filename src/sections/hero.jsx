8e72cefddc8a433ac037fa6740f14d5a88f61871
Nikeeyyy <hk5561111212@gmail.com>
2023-11-24 01:18:43 +0530

    page load

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -1,70 +1,124 @@
-import React, { useLayoutEffect } from 'react'
-import { useRef } from 'react'
-import chrome from '../assets/Assets/web.png'
+import React, { useEffect, useLayoutEffect, useState } from "react";
+import { useRef } from "react";
 import { gsap } from "gsap";
+import backgroundInage from "../assets/Assets/main-image.jpg";
 
 const Hero = () => {
+  const mainText = useRef();
+  const backgroundImg = useRef();
 
-  const mainText = useRef(); 
-  
   useLayoutEffect(() => {
     var tl = gsap.timeline();
 
-    //sequenced one-after-the-other
-    tl.fromTo(".anime_top-text span", {
-       opacity: 0,
-       y: 50,
-       transformOrigin: 'bottom'
-    },{
-      opacity: 1,
-      duration: 1,
-      ease: "power2.inOut",
-      delay: 1,
-      y: 0
-    })
-    .fromTo(".anime_second_text span", {
-      opacity: 0,
-      transformOrigin: 'left',
-      y: 50,
-    },{
-     opacity: 1,
-     duration: 1,
-     ease: "power2.inOut",
-     y: 0,
-    })
-    .fromTo(".rotating_image", {x: "-1500px", opacity: 0},{x: 0, opacity:1, duration: 1})
-    .fromTo(".anime_last_one span", {
-      opacity: 0,
-      transformOrigin: 'left',
-      y: 50,
-    },{
-     opacity: 1,
-     duration: 1,
-     ease: "power2.inOut",
-     y: 0
-    }
-    ).to(".rotating_image", {
-      rotation: 360,
-      duration: 1,
-      repeat: 1,
-      repeatDelay: 1,
-      repeat: -1,
-      ease: "power2.inOut",
-    })
-  }, [])
+    tl.fromTo(
+      ".anime_top-text span",
+      {
+        opacity: 0,
+        y: 50,
+        transformOrigin: "bottom",
+      },
+      {
+        opacity: 1,
+        duration: 1,
+        ease: "power2.inOut",
+        delay: 1,
+        y: 0,
+      }
+    )
+      .fromTo(
+        ".anime_second_text span",
+        {
+          opacity: 0,
+          transformOrigin: "left",
+          y: 50,
+        },
+        {
+          opacity: 1,
+          duration: 1,
+          ease: "power2.inOut",
+          y: 0,
+        },
+        "-=0.5"
+      )
+      // .fromTo(
+      //   ".rotating_image",
+      //   { x: "-1500px", opacity: 0 },
+      //   { x: 0, opacity: 1, duration: 1 }
+      // )
+      .fromTo(
+        ".anime_last_one span",
+        {
+          opacity: 0,
+          transformOrigin: "left",
+          y: 50,
+        },
+        {
+          opacity: 1,
+          duration: 1,
+          ease: "power2.inOut",
+          y: 0,
+        },
+        "-=0.5"
+      );
+    // .to(".rotating_image", {
+    //   rotation: 360,
+    //   duration: 1,
+    //   repeat: 1,
+    //   repeatDelay: 1,
+    //   repeat: -1,
+    //   ease: "power2.inOut",
+    // });
+  }, []);
 
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const count = setCounter(() => {
+      setCounter((counter) => {
+        counter > 100 ? counter + 1 : clearInterval(count), setCounter(100);
+      });
+    }, 25);
+  }, []);
 
   return (
-       <div className="hero_container h-screen">
-<div className="hero_wrapper page_width flex justify-center flex-col h-full">
-    <h1 ref={mainText} className="text-4xl leading-relaxed	 font-[Liber] font-bold anime_top-text mb-5 overflow-hidden"><span className="inline-block">Hello I'm Nikhil</span></h1>
-    <h2 className="text-8xl font-bold anime_second_text overflow-hidden"> <span className="inline-block">I'm a Cook &</span></h2>
-    <h2 className="text-8xl leading-[5.5rem] pb-4 font-bold flex relative anime_last_one overflow-hidden"><span className="inline-block">I Cook things for web</span>
-        <img className="rotating_image ml-6" src={chrome} alt="chrome-image" />
-    </h2>
-</div>
- </div>
-  )
-}
+    <>
+      <div className="fixed top-0 left-0 bg-red-400 h-full w-full z-[999999] flex flex-col justify-center items-center">
+        <div
+          className="bg-black h-[2px] w-0"
+          style={{ width: counter + "%" }}
+        ></div>
+        {counter}
+      </div>
+      <div className="hero_container h-screen">
+        <div className="hero_wrapper page_width flex justify-center flex-col h-full">
+          <img
+            ref={backgroundImg}
+            className="absolute right-0 top-0 h-full object-cover w-1/2"
+            src={backgroundInage}
+            alt="background image"
+          />
+          <h1
+            ref={mainText}
+            className="text-8xl font-[Anton] font-bold anime_top-text mb-5 overflow-hidden"
+          >
+            <span className="inline-block">Hello I'm Nikhil</span>
+          </h1>
+          <h2 className="text-8xl font-[Anton] font-bold anime_second_text mb-5 overflow-hidden">
+            {" "}
+            <span className="inline-block">I'm a Cook &</span>
+          </h2>
+          <h2 className="text-8xl font-[Anton] pb-4 font-bold flex relative anime_last_one overflow-hidden">
+            <span className="inline-block">I Cook things for web</span>
+            {/* <img
+            className="rotating_image ml-6"
+            src={chrome}
+            alt="chrome-image"
+          /> */}
+          </h2>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default Hero
+export default Hero;
--- src/sections/navbar.jsx
+++ src/sections/navbar.jsx
@@ -7,52 +7,55 @@ import ScrollTrigger from 'gsap/ScrollTrigger'
 
 
 const Navbar = () => {
+  gsap.registerPlugin(ScrollTrigger);
 
-    gsap.registerPlugin(ScrollTrigger)
-
-    useLayoutEffect(()=> {
-        gsap.to(".animate_header", {
-            background: "#f9f9f9",
-            color: "#000",
-            duration: 2,
-            scrollTrigger: {
-              trigger: ".animate_header",
-              start: "100px top",
-              end: "bottom top",
-              scrub: 2, 
-            //   markers: true,
-              triggerActions: "restart none reverse none "
-            },
-          })
-    }, [])
+  // useLayoutEffect(()=> {
+  //     gsap.to(".animate_header", {
+  //         background: "#f9f9f9",
+  //         color: "#000",
+  //         duration: 2,
+  //         scrollTrigger: {
+  //           trigger: ".animate_header",
+  //           start: "100px top",
+  //           end: "bottom top",
+  //           scrub: 2,
+  //         //   markers: true,
+  //           triggerActions: "restart none reverse none "
+  //         },
+  //       })
+  // }, [])
 
   return (
-      <div className="h-[70px] sticky top-0 animate_header z-10">
-    <div className="page_width h-full flex items-center">
+    <div className="h-[70px] sticky top-0 animate_header z-[9999] bg-[#0E100F]">
+      <div className="page_width h-full flex items-center">
         <div className="logo_container flex items-center gap-2 w-[20%]">
-            <img height="50" width="50" src={logo} alt="logo" className="logo"/>
-            <span className="font-[Liber] text-xs">Nxkeyyy</span>
+          <img height="50" width="50" src={logo} alt="logo" className="logo" />
+          <span className="font-[General Sans] text-xl font-semibold">
+            Nxkeyyy
+          </span>
         </div>
-        <ul className="flex w-[60%] m-auto justify-center gap-4 h-full items-center anime-links">
-            <li>
-                <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#about">About</a>
-            </li>
-            <li>
-                <a href="#">Projects</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
+        <ul className="flex w-[80%] m-auto justify-end gap-8 h-full items-center anime-links">
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a
+              className="bg-green-300 px-8 py-3 rounded-full text-black"
+              href="#"
+            >
+              Contact
+            </a>
+          </li>
         </ul>
-        <div className="w-[20%]">
-
-        </div>
+      </div>
     </div>
- </div>
-  )
+  );
 }
 
 export default Navbar
