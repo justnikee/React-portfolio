ffad52380de6e5da9c1bdf99648644f92388891c
Nikeeyyy <hk5561111212@gmail.com>
2023-11-25 18:24:06 +0530

    preloader

--- src/sections/hero.jsx
+++ src/sections/hero.jsx
@@ -4,90 +4,129 @@ import { gsap } from "gsap";
 import backgroundInage from "../assets/Assets/main-image.jpg";
 
 const Hero = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const startLoader = () => {
+    if (counter === 100) {
+      return;
+    }
+
+    setCounter((prevCounter) => {
+      const newCounter = prevCounter + Math.floor(Math.random() * 10) + 1;
+
+      if (newCounter > 100) {
+        return 100;
+      }
+
+      return newCounter;
+    });
+
+    let delay = Math.floor(Math.random() * 200) + 50;
+
+    setTimeout(startLoader, delay);
+  };
+
+  useEffect(() => {
+    // Call startLoader to initiate the counter update
+    startLoader();
+  }, []); // Empty dependency array to run the effect only once on mount
+
   const mainText = useRef();
   const backgroundImg = useRef();
+  const loadingScreen = useRef();
+  const text = useRef();
+  const bar = useRef();
 
   useLayoutEffect(() => {
-    var tl = gsap.timeline();
+    gsap.registerPlugin();
 
-    tl.fromTo(
-      ".anime_top-text span",
+    const timeline = gsap.timeline();
+
+    timeline.to(text.current, {
+      ease: "power2.inOut",
+    });
+
+    timeline.to(loadingScreen.current, {
+      height: 0,
+      duration: 1,
+      delay: 3,
+    });
+
+    timeline.to(
+      [text.current, bar.current],
       {
         opacity: 0,
+      },
+      "-=2"
+    );
+
+    timeline.fromTo(
+      ".anime_top-text span",
+      {
+        autoAlpha: 0,
         y: 50,
         transformOrigin: "bottom",
       },
       {
-        opacity: 1,
+        autoAlpha: 1,
         duration: 1,
         ease: "power2.inOut",
         delay: 1,
         y: 0,
       }
-    )
-      .fromTo(
-        ".anime_second_text span",
-        {
-          opacity: 0,
-          transformOrigin: "left",
-          y: 50,
-        },
-        {
-          opacity: 1,
-          duration: 1,
-          ease: "power2.inOut",
-          y: 0,
-        },
-        "-=0.5"
-      )
-      // .fromTo(
-      //   ".rotating_image",
-      //   { x: "-1500px", opacity: 0 },
-      //   { x: 0, opacity: 1, duration: 1 }
-      // )
-      .fromTo(
-        ".anime_last_one span",
-        {
-          opacity: 0,
-          transformOrigin: "left",
-          y: 50,
-        },
-        {
-          opacity: 1,
-          duration: 1,
-          ease: "power2.inOut",
-          y: 0,
-        },
-        "-=0.5"
-      );
-    // .to(".rotating_image", {
-    //   rotation: 360,
-    //   duration: 1,
-    //   repeat: 1,
-    //   repeatDelay: 1,
-    //   repeat: -1,
-    //   ease: "power2.inOut",
-    // });
-  }, []);
+    );
 
-  const [counter, setCounter] = useState(0);
+    timeline.fromTo(
+      ".anime_second_text span",
+      {
+        autoAlpha: 0,
+        transformOrigin: "left",
+        y: 50,
+      },
+      {
+        autoAlpha: 1,
+        duration: 1,
+        ease: "power2.inOut",
+        y: 0,
+      },
+      "-=0.5"
+    );
 
-  useEffect(() => {
-    const count = setCounter(() => {
-      setCounter((counter) => {
-        counter > 100 ? counter + 1 : clearInterval(count), setCounter(100);
-      });
-    }, 25);
+    timeline.fromTo(
+      ".anime_last_one span",
+      {
+        autoAlpha: 0,
+        transformOrigin: "left",
+        y: 50,
+      },
+      {
+        autoAlpha: 1,
+        duration: 1,
+        ease: "power2.inOut",
+        y: 0,
+      },
+      "-=0.5"
+    );
   }, []);
 
   return (
     <>
-      <div className="fixed top-0 left-0 bg-red-400 h-full w-full z-[999999] flex flex-col justify-center items-center">
+      <div
+        ref={loadingScreen}
+        className="fixed top-0 left-0 bg-yellow-600 h-full w-full z-[999999]"
+      >
         <div
-          className="bg-black h-[2px] w-0"
+          ref={bar}
+          className="bg-black h-[100px] w-0 fixed bottom-0 left-0 ease-in-out"
           style={{ width: counter + "%" }}
         ></div>
-        {counter}
+        <h2
+          className="fixed top-1/2 left-1/2 text-black text-6xl font-[Liber] translate-x-[-50%] translate-y-[-50%]	"
+          ref={text}
+        >
+          {counter}
+        </h2>
       </div>
       <div className="hero_container h-screen">
         <div className="hero_wrapper page_width flex justify-center flex-col h-full">
@@ -109,11 +148,6 @@ const Hero = () => {
           </h2>
           <h2 className="text-8xl font-[Anton] pb-4 font-bold flex relative anime_last_one overflow-hidden">
             <span className="inline-block">I Cook things for web</span>
-            {/* <img
-            className="rotating_image ml-6"
-            src={chrome}
-            alt="chrome-image"
-          /> */}
           </h2>
         </div>
       </div>
